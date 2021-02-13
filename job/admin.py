244178befaf6ee9b43455304d4b61a6d138af525
= <=>
2021-02-14 00:23:05 +0300

    add job models

--- job/admin.py
+++ job/admin.py
@@ -3,4 +3,4 @@ from django.contrib import admin
 # Register your models here.
 from .models import Job 
 
-admin.site.register(Job)
+admin.site.register(Job) # Add Job To Admin Panel For Django Admin
--- job/models.py
+++ job/models.py
@@ -2,11 +2,27 @@ from django.db import models
 
 """
 django model field
-	- html widget
+	- html widget (Done)
 	- validation
-	- db size
+	- db size (Max Length And Min Length)
 """
 
 # Create your models here.
+
+JOB_TYPE = (
+	("FULL TIME", "FULL TIME"),
+	("PART TIME", "PART TIME"),
+)
+
 class Job(models.Model): # table
 	title = models.CharField(max_length=100) # column
+	# location
+	job_type = models.CharField(max_length=15, choices=JOB_TYPE) # column
+	description = models.TextField(max_length=1000) # column
+	published_at = models.DateTimeField(auto_now=True) # column
+	Vacancy = models.IntegerField(default=1) # column
+	salary = models.IntegerField(default=0) # column
+	expereince = models.IntegerField(default=1) # column
+
+	def __str__(self):
+		return f"{self.title} | Expereince {self.expereince} Year"
